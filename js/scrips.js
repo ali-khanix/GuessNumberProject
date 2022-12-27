f1861463474e9cda2864cdb50c2c510c44b3d193
Ali Khani <khaniali568@gmail.com>
2022-12-27 03:56:04 +0330

    Refactor the code

--- js/scrips.js
+++ js/scrips.js
@@ -4,22 +4,26 @@ const message = document.querySelector('.message');
 const number = document.querySelector('.number');
 const highScoreTxt = document.querySelector('.high-score');
 
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
 const btnCheck = document.querySelector('.check');
 const btnAgain = document.querySelector('.again');
 
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(randomNumber);
 
 let score = 20;
 let highScore = 0;
 
+// Check button
 btnCheck.addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    message.textContent = 'لطفا یک عدد وارد کنید';
+    displayMessage('لطفا یک عدد وارد کنید');
   } else if (guess === randomNumber) {
-    message.textContent = 'درست حدس زدی';
+    displayMessage('درست حدس زدی');
     number.textContent = randomNumber;
     document.body.classList.add('body-background');
 
@@ -27,38 +31,28 @@ btnCheck.addEventListener('click', () => {
       highScore = score;
       highScoreTxt.textContent = score;
     }
-  } else if (guess > randomNumber) {
+  } else if (guess !== randomNumber) {
     if (score > 1) {
-      message.textContent = 'حدست زیاد بود';
+      displayMessage(guess > randomNumber ? 'حدست زیاد بود' : 'حدست کم بود');
+
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      message.textContent = ' باختی';
-      document.querySelector('.score').textContent = 0;
-      document.body.style.backgroundColor = 'red';
-    }
-  } else if (guess < randomNumber) {
-    if (score > 1) {
-      message.textContent = 'حدست کم بود';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      message.textContent = ' باختی';
+      displayMessage('باختی');
       document.querySelector('.score').textContent = 0;
       document.body.style.backgroundColor = 'red';
     }
   }
 });
 
+// Again Button
 btnAgain.addEventListener('click', () => {
   score = 20;
   randomNumber = Math.trunc(Math.random() * 20) + 1;
-  message.textContent = '...شروع به حدس زدن کن';
+  displayMessage('...شروع به حدس زدن کن');
   document.querySelector('.score').textContent = score;
   number.textContent = '?';
   document.querySelector('.guess').value = Number('');
   document.body.classList.remove('body-background');
   document.body.style.backgroundColor = '';
-
-  console.log(randomNumber);
 });
